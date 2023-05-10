c006c49d6a4045ffeda1740bf18e52c788d3b3d1
Caroline Guerra <caroline-guerra@hotmail.com>
2023-05-10 15:16:32 -0300

    Initial Commit

---
+++ WallpaperGallery/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  WallpaperGallery
+//
+//  Created by Caroline Guerra on 10/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
